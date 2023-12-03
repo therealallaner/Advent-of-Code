b855e72a0d5fe7b8bf2084f932c2b8c4117d884b
allaner <atj3jjj@hotmail.com>
2023-12-03 15:17:25 -0600

    Day 3 Part 1
    Finished Day 3 part 1 but not part 2.

--- 2023 Advent Calender/Day 3/Day 3.py
+++ 2023 Advent Calender/Day 3/Day 3.py
@@ -1,6 +1,13 @@
-from utils.decorators import time_it
 
-Day_3 = ""
+Day_3 = "Gear Ratios"
+
+
+symbol_coords = []
+hot_spots = []
+adjacent_nums = ['0']
+sum_num = 0
+line_count = 0
+
 
 def file_to_list():
     return_list = []
@@ -16,5 +23,84 @@ def file_to_list():
 data = file_to_list()
 
 
-print(data)
+for line in data:
+    line_count += 1
+    for i, char in enumerate(line):
+        if char != ".":
+            if not char.isnumeric():
+                symbol_coords.append(f"{i} {line_count}")
 
+
+for line in symbol_coords:
+    line = line.split()
+    x = int(line[0])
+    y = int(line[-1])
+    hot_spots.append(f"{x-1} {y}")
+    hot_spots.append(f"{x-1} {y-1}")
+    hot_spots.append(f"{x} {y-1}")
+    hot_spots.append(f"{x+1} {y-1}")
+    hot_spots.append(f"{x+1} {y}")
+    hot_spots.append(f"{x+1} {y+1}")
+    hot_spots.append(f"{x} {y+1}")
+    hot_spots.append(f"{x-1} {y+1}")
+
+
+line_count = 0
+
+
+for line in data:
+    line_count += 1
+    one_ago = ""
+    two_ago = ""
+
+    for i, char in enumerate(line):
+
+        if f"{i} {line_count}" in hot_spots:
+            if char.isnumeric():
+
+#               (..xxx)
+                if line[i+1].isnumeric() and line[i+2].isnumeric():
+                    adjacent_nums.append(f"{char}{line[i+1]}{line[i+2]}")
+                    #print(f"{char}{line[i+1]}{line[i+2]}")
+
+#               (.xxx.)
+                if one_ago.isnumeric() and line[i+1].isnumeric():
+                    if int(f"{one_ago}{char}{line[i+1]}") != int(adjacent_nums[-1]):
+                        #print(adjacent_nums[-2], adjacent_nums[-1],f"{one_ago}{char}{line[i+1]}")
+                        adjacent_nums.append(f"{one_ago}{char}{line[i+1]}")
+
+#               (xxx..)
+                if two_ago.isnumeric() and one_ago.isnumeric():
+                    if int(f"{two_ago}{one_ago}{char}") != int(adjacent_nums[-1]):
+                        adjacent_nums.append(f"{two_ago}{one_ago}{char}")
+                        #print(f"{two_ago}{one_ago}{char}")
+
+#               (..xx.)
+                if not one_ago.isnumeric() and line[i+1].isnumeric() and not line[i+2].isnumeric():
+                    adjacent_nums.append(f"{char}{line[i+1]}")
+                    #print(f"{char}{line[i+1]}")
+
+#               (.xx..)
+                if not two_ago.isnumeric() and one_ago.isnumeric() and not line[i+1].isnumeric():
+                    if int(f"{one_ago}{char}") != int(adjacent_nums[-1]):
+                        adjacent_nums.append(f"{one_ago}{char}")
+                        #print(f"{one_ago}{char}")
+
+#               (..x..)
+                if not one_ago.isnumeric() and not line[i+1].isnumeric():
+                    adjacent_nums.append(char)
+                    #print(char)
+
+
+        two_ago = one_ago
+        one_ago = char
+
+
+for line in adjacent_nums:
+    sum_num += int(line)
+
+
+print("Part 1 =", sum_num)
+
+
+# Part 2 is probably not solvable using this strategy. I think Ima have to completely rewrite it.
